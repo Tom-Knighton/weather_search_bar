f2b8809db58ec06b90c638e6a5e78a9ff1c8f8e5
martaweronikamw <101061129+martaweronikamw@users.noreply.github.com>
2022-03-25 13:32:20 +0000

    done deal

---
+++ searchBar.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import TextField from "@mui/material/TextField";
+import fixingData from "./fixingData.js"
+
+function SearchBar(){
+    return (
+
+        <div classname = "searchbar">
+            <TextField label="Search" 
+            />
+
+        </div>
+    );
+
+}
+
+export default SearchBar;
